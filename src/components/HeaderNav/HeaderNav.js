6db182967f9bf9b94b289044556b1d3a4c4886f5
M-Joyce <Mark4751@gmail.com>
2021-11-12 12:23:19 -0700

    added link to map in navbar

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -5,11 +5,12 @@ import "./HeaderNav.css"
 function HeaderNav() {
     return (
         <Navbar bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">Home</Navbar.Brand>
+            <Container className="headerNav">
+                <Navbar.Brand href="#">Home</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="#howToPlay">How to Play</Nav.Link>
                     <Nav.Link href="#discord">Discord</Nav.Link>
+                    <Nav.Link target="blank" href="http://play.regionbattle.com:8123/">SMP Map</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
